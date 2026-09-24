d2f16aed2d857b2d10c25899e12ce59f2a8748eb
jm_gossani <migueljm76@outlook.com>
2026-09-24 08:10:44 -0300

    f

--- bd-scholarregular/screens/api.js
+++ bd-scholarregular/screens/api.js
@@ -1,1 +1,18 @@
-export const API_URL = 'http://172.26.64.1/app_scholar_api';
+export const API_UR = 'http://172.26.64.1/app_scholar_api';
+
+
+
+const API_URL = 'https://api-scholar.migueljm76.workers.dev';
+
+// Função para buscar todos os alunos (GET)
+export async function getAlunos() {
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) throw new Error('Erro na requisição');
+    return await response.json();
+  } catch (error) {
+    console.error('Erro ao buscar alunos:', error);
+    return [];
+  }
+}
+
